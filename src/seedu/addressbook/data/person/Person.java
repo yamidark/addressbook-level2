c79b0790313cab520320c152a8a6593e2fa58365
yamidark <E0002937@u.nus.edu>
2016-09-06 00:44:18 +0800

    Add class level members

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -14,8 +14,11 @@ public class Person implements ReadOnlyPerson {
     private Phone phone;
     private Email email;
     private Address address;
-
+    
     private final UniqueTagList tags;
+    
+    private int nextSequenceNumber = 1;
+    private int sequenceNumber;
     /**
      * Assumption: Every field must be present and not null.
      */
@@ -25,6 +28,7 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
+        sequenceNumber = nextSequenceNumber++;
     }
 
     /**
